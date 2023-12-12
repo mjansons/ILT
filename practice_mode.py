5ebce762ea73f913402c9600b71bb8703a580517
Matiss Jansons <matiss93jansons@gmail.com>
2023-12-12 21:51:23 +0100

    weighted question selection

--- practice_mode.py
+++ practice_mode.py
@@ -1,6 +1,10 @@
+from dataclasses import dataclass
+import random
+import question_stats
+
 # A mode in which questions are given non-stop so that the user can practice. 
 
-# Qquestions that are answered correctly become less likely to appear,
+# Questions that are answered correctly become less likely to appear,
 
 # while questions that are answered incorrectly become more likely to appear.
 # Hint: you may want to look into weighted random choices. 
@@ -32,3 +36,81 @@
 # if answered incorrectly:
     # times shown +1
         # new_times_shown = times shown + 1
+
+
+
+
+
+#     # - question selector
+#     # SELECT A QUESTION FROM ALL ACTIVE QUESTIONS
+
+@dataclass
+class QuestionPresenter:
+
+    @staticmethod
+    def select_weighted_question(the_list):
+        # filter active questions
+        active_questions = [question for question in the_list if question["status"] == "active"]
+        # get percentage data 
+        list_of_percentages = [question["answered"] for question in active_questions]
+        # invert values and make sure they always at least +0.01
+        weights = [abs(0.99 - float(percentage)) for percentage in list_of_percentages]
+        # getting the question
+        selected_question = random.choices(active_questions, weights=weights, k=1)
+        return selected_question
+
+
+        
+
+
+
+#     # - question asker
+#     # ASK THAT QUESTION
+
+#     # - answer evaluator
+#     # EVALUATE ANSWER
+#     # UPDATE THE DICT
+
+        #value = 3.14159265359
+        # #Round to 2 digits after the decimal point
+        # rounded_value = round(value, 2)
+
+#     # GIVE FEEDBACK - Whether it was correct or not and if not what was the right answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # LOOP
+# while True:
+#     # GET THE LIST from FILE
+#     my_list = question_stats.My_csv_manager.make_dict("questions.csv")
+
+#     # - question selector
+#     # SELECT A QUESTION FROM ALL ACTIVE QUESTIONS
+
+#     # - question asker
+#     # ASK THAT QUESTION
+
+#     # - answer evaluator
+#     # EVALUATE ANSWER
+#     # UPDATE THE DICT
+#     # GIVE FEEDBACK - Whether it was correct or not and if not what was the right answer
+
+#     # RE_WRITE THE FILE
+#     question_stats.My_csv_manager.re_write_csv(my_list, "questions.csv")
+
+
+
+
+
